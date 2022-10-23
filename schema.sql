e6fac517530ec532f27d7070d9b9f306830123bb
Marcin Szajek <marcin.szajek+github@gmail.com>
2022-10-23 22:03:12 +0200

    Create also DB, not only tables

--- schema.sql
+++ schema.sql
@@ -1,2 +1,5 @@
+CREATE DATABASE IF NOT EXISTS product;
+USE product;
+
 CREATE TABLE IF NOT EXISTS ps_stock_available (id_product INT, quantity INT);
 CREATE TABLE IF NOT EXISTS ps_product (id_product INT, reference VARCHAR(100));
